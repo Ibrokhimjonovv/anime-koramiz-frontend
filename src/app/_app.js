4db6a1cc42be6bdd48b55e2f4704bd76be21a7df
Ibrokhimjonovv <megabytekekabyte@gmail.com>
2025-11-22 02:37:49 +0500

    Initial commit

--- src/app/_app.js
+++ src/app/_app.js
@@ -3,10 +3,10 @@ import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import "../../styles/global.scss";
 
-export const global_api = "http://192.168.1.15:8000/watch-anime/api";
+export const global_api = "https://bk.tarjima-animelar.uz/watch-anime/api";
 export const global_proxy = "https://proxy.afd-platform.uz/proxy?url=";
 export const global_domen = "http://192.168.1.15:8000";
-export const global_api_ssr = 'http://192.168.1.15:8000/watch-anime/api'
+export const global_api_ssr = 'https://bk.tarjima-animelar.uz/watch-anime/api'
 
 function MyApp({ Component, pageProps }) {
   return (
